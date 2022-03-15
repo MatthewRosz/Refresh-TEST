94d6faef2f041e8cddfe6e802dde5f9bfb7b4e5b
Your Name <you@example.com>
2022-03-15 16:49:28 +0100

    js test basic

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-console.log("Hello on my test page!")
+//console.log("Hello on my test page!")
 
-let total = 3 + 4
+
+// let userAge = prompt("Enter your age here:")
+
+// if (userAge >= 18) {
+// console.log("You over 18, Welcome")
+// }
+// else if (userAge < 18) {
+//     console.log("Sorry, you can not be here, get out!")
+// }
+
+/* const age = prompt("Enter your age:")
+
+if (age < 5) {
+    console.log("You pass for Free kid!")
+} else if (age < 10) {
+    console.log("Pay 10$")
+} else if (age < 65) {
+    console.log("Good morning, 20$ please")
+} else if (age >= 65) {
+    console.log("Senior, you pay only 10$")
+} */
+
+const password = prompt("Please enter new password here:")
+
+if (password.length >= 6) {
+    if (password.indexOf(" ") === -1) {
+        console.log("Valid password!")
+    } else {
+        console.log("Remove spaces!")
+    }
+} else {
+    console.log("Password must have more than 6 chararters")
+}
+
